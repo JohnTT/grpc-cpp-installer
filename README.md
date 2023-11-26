316c657178a60320842a785b64e8af258ce0fa5e
John Chen <jmc_tt@live.com>
2023-11-25 20:30:06 -0500

    Add README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# Instructions
+Simply run `./install.sh` and gRPC C++ will be installed to $HOME/.local for use.  
+Alternatively, you can pipe the entire script to your shell without even checking out this repo.
+```
+curl -s https://raw.githubusercontent.com/JohnTT/grpc-cpp-installer/main/install.sh | bash
+```
--- install.sh
+++ install.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-
 export MY_INSTALL_DIR=$HOME/.local
 if [ -d "$MY_INSTALL_DIR" ]; then
     echo "Error: gRPC install directory $MY_INSTALL_DIR exists."
     exit 1
 fi
 
-mkdir -p $MY_INSTALL_DIR
+echo "Installing gRPC C++ to $MY_INSTALL_DIR"
+sudo mkdir -p $MY_INSTALL_DIR
 export PATH="$MY_INSTALL_DIR/bin:$PATH"
+
+echo "Installing gRPC C++ dependencies (cmake build-essential autoconf libtool pkg-config)"
+sudo apt install -y cmake build-essential autoconf libtool pkg-config
+sudo -k
 
 GRPC_VERSION=$(git ls-remote --tags https://github.com/grpc/grpc | \
     grep 'refs/tags/v[0-9]*\.[0-9]*\.[0-9]*$' | \
     sort -t '/' -k 3 -V | tail -n 1 | awk -F'/' '{print $3}')
-echo "Installing gRPC version $GRPC_VERSION to $MY_INSTALL_DIR"
-
-echo "Installing gRPC C++ dependencies"
-sudo apt install -y cmake build-essential autoconf libtool pkg-config
-
-echo "Cloning gRPC repository at $GRPC_VERSION"
+echo "Cloning gRPC repository at version $GRPC_VERSION"
 git clone --recurse-submodules -b $GRPC_VERSION --depth 1 --shallow-submodules https://github.com/grpc/grpc
 
 echo "Building gRPC C++"
